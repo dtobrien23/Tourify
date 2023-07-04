663915e9e1ebd604b7d2230826056fdac6d77458
Dean <dtobrien23@gmail.com>
2023-07-04 12:52:34 +0100

    add button to start routing

--- Frontend/src/components/SearchBar.js
+++ Frontend/src/components/SearchBar.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import { Flex, Divider } from '@chakra-ui/react';
+import { Flex, Divider, Button } from '@chakra-ui/react';
 import DestinationInput from './DestinationInput';
 import LocationInput from './LocationInput';
 
 export default function SearchBar({ map }) {
   return (
-    <Flex
-      style={{
-        width: 'fit-content',
-        border: 'solid 2px orangered',
-        borderRadius: '20px',
-        backgroundColor: 'white',
-        zIndex: 1,
-        height: '38px',
-      }}
-    >
-      <DestinationInput map={map} style={{ zIndex: 2 }} />
-      <Divider orientation="vertical" />
-      <LocationInput map={map} />
+    <Flex>
+      <Flex
+        style={{
+          width: 'fit-content',
+          border: 'solid 2px orangered',
+          borderRadius: '20px',
+          backgroundColor: 'white',
+          zIndex: 1,
+          height: '38px',
+        }}
+      >
+        <LocationInput map={map} />
+        <Divider orientation="vertical" />
+        <DestinationInput map={map} style={{ zIndex: 2 }} />
+      </Flex>
+      <Button
+        ml={2}
+        bg="gold"
+        color="white"
+        border={'solid 2px orangered'}
+        borderRadius={20}
+      >
+        Tourify Me
+      </Button>
     </Flex>
   );
 }
