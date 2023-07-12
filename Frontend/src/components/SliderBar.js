7dc1e4ed4cfe50dd98a5c063e69e7728c4cd9fad
Dean <dtobrien23@gmail.com>
2023-07-12 15:22:11 +0100

    style busyness slider with busyness icons

--- Frontend/src/components/SliderBar.js
+++ Frontend/src/components/SliderBar.js
@@ -4,13 +4,14 @@ import {
   RangeSliderTrack,
   RangeSliderFilledTrack,
   RangeSliderThumb,
+  RangeSliderMark,
 } from '@chakra-ui/react';
 import { Flex } from '@chakra-ui/react';
 //import attractions from '../static/attractions.json';
 import { APIContext } from './APIContext';
 
 export default function SliderBar({ setSliderListFunc }) {
-  const { apiAttractions, apiLoaded} = useContext(APIContext);
+  const { apiAttractions, apiLoaded } = useContext(APIContext);
 
   const [sliderValue, setSliderValue] = useState([0, 100]);
   const [filteredAttractions, setFilteredAttractions] =
@@ -53,6 +54,31 @@ export default function SliderBar({ setSliderListFunc }) {
         flexDirection: 'column',
       }}
     >
+      {/* <button
+        style={{
+          // width: 'fit-content',
+          width: '90px',
+          marginTop: '10px',
+          marginBottom: '10px',
+          // padding: '5px',
+          // paddingRight: '10px',
+          // paddingLeft: '10px',
+          border: 'solid 2px white',
+          borderRadius: '20px',
+          background: 'white',
+          // color: 'orangered',
+          boxShadow: '1px 1px 5px 1px rgba(0, 0, 0, 0.6)',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <img
+          src="/images/busyness-slider/busy.svg"
+          alt="Busyness Slider"
+          style={{ height: '45px', width: '45px' }}
+        />
+      </button>
       <button
         style={{
           // width: 'fit-content',
@@ -73,11 +99,36 @@ export default function SliderBar({ setSliderListFunc }) {
         }}
       >
         <img
-          src="/images/busyness-slider-icon.png"
+          src="/images/busyness-slider/kinda-busy.svg"
           alt="Busyness Slider"
           style={{ height: '45px', width: '45px' }}
         />
       </button>
+      <button
+        style={{
+          // width: 'fit-content',
+          width: '90px',
+          marginTop: '10px',
+          marginBottom: '10px',
+          // padding: '5px',
+          // paddingRight: '10px',
+          // paddingLeft: '10px',
+          border: 'solid 2px white',
+          borderRadius: '20px',
+          background: 'white',
+          // color: 'orangered',
+          boxShadow: '1px 1px 5px 1px rgba(0, 0, 0, 0.6)',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <img
+          src="/images/busyness-slider/not-busy.svg"
+          alt="Busyness Slider"
+          style={{ height: '45px', width: '45px' }}
+        />
+      </button> */}
       <Flex
         style={{
           border: 'solid 2px white',
@@ -91,15 +142,62 @@ export default function SliderBar({ setSliderListFunc }) {
           defaultValue={[0, 100]}
           orientation="vertical"
           minH="505"
-          minWidth={'20'}
+          maxW="20"
           position={'right'}
-          border={'solid 35px white'}
+          border={'solid 15px white'}
+          borderRight={'solid 30px white'}
           borderRadius={'20px'}
           backgroundColor={'white'}
           step={1}
           // call function as slider moves
           onChangeEnd={handleSliderChange}
+          sx={{
+            width: '30px', // Adjust the width of the slider container
+            '.chakra-range__track': {
+              right: '10px', // Adjust the position of the slider track from the right side
+            },
+          }}
         >
+          <RangeSliderMark
+            value={0}
+            mt="1"
+            pr="-10px"
+            mr="-50px" // Adjust the left margin for positioning
+            fontSize="sm"
+            position="absolute" // Set position to absolute
+          >
+            <img
+              src="/images/busyness-slider/not-busy.svg"
+              alt="Busyness Slider"
+              style={{ height: '45px', width: '45px' }}
+            />
+          </RangeSliderMark>
+          <RangeSliderMark
+            value={50}
+            mt="1"
+            mr="-50px" // Adjust the left margin for positioning
+            fontSize="sm"
+            position="absolute" // Set position to absolute
+          >
+            <img
+              src="/images/busyness-slider/kinda-busy.svg"
+              alt="Busyness Slider"
+              style={{ height: '45px', width: '45px' }}
+            />
+          </RangeSliderMark>
+          <RangeSliderMark
+            value={90}
+            mt="1"
+            mr="-50px" // Adjust the left margin for positioning
+            fontSize="sm"
+            position="absolute" // Set position to absolute
+          >
+            <img
+              src="/images/busyness-slider/busy.svg"
+              alt="Busyness Slider"
+              style={{ height: '40px', width: '40px' }}
+            />
+          </RangeSliderMark>
           <RangeSliderTrack>
             <RangeSliderFilledTrack />
           </RangeSliderTrack>
