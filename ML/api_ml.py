88808c7c2ff8542c8f832d947932453cb8ff22b8
Anastasiia Skrypnykova <46396153+natasiia@users.noreply.github.com>
2023-07-17 21:55:53 +0100

    Added Flask app for ML API calls

---
+++ ML/api_ml.py
@@ -0,0 +1,66 @@
+import sys
+from flask import Flask, request, jsonify
+import traceback
+import pandas as pd
+import joblib
+import numpy as np
+import sklearn
+
+# Import the models dictionary from models.py
+from models import models
+
+# Your API definition
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        json_ = request.json
+        print(json_)
+
+        # Extract the input parameters from the JSON data
+        month = json_['month']
+        day_of_week = json_['day_of_week']
+        hour = json_['hour']
+        taxi_locationID = json_['taxi_locationID']
+        passengers = json_['passengers']
+        temp_avg = json_['temp_avg']
+        precipitation = json_['precipitation']
+
+        # Load the corresponding model based on the taxi_locationID
+        model = models[taxi_locationID]
+
+        # Create a new DataFrame with the input data for prediction
+        new_data = pd.DataFrame({
+            'month': [month],
+            'day_of_week': [day_of_week],
+            'hour': [hour],
+            'taxi_locationID': [taxi_locationID],
+            'passengers': [passengers],
+            'temp_avg': [temp_avg],
+            'precipitation': [precipitation]
+        })
+
+        # Use the loaded model to make predictions
+        predictions = model.predict(new_data)
+
+        # Round the prediction to the nearest whole number
+        prediction = round(predictions[0])
+        
+        # If prediction is greater than 100, set it to 100
+        if prediction > 100:
+            prediction = 100
+
+        return jsonify({'prediction': prediction})
+
+    except:
+        return jsonify({'trace': traceback.format_exc()})
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1])  # This is for a command-line input
+    except:
+        port = 12345  # If you don't provide any port, the port will be set to 12345
+
+    print('Models loaded')
+    app.run(port=port, debug=True)
---
+++ ML/models.py
@@ -0,0 +1,24 @@
+import joblib
+
+# Load the models
+models = {
+    42: joblib.load('pickle_files/42.pkl'),
+    43: joblib.load('pickle_files/43.pkl'),
+    45: joblib.load('pickle_files/45.pkl'),
+    88: joblib.load('pickle_files/88.pkl'),
+    100: joblib.load('pickle_files/100.pkl'),
+    142: joblib.load('pickle_files/142.pkl'),
+    148: joblib.load('pickle_files/148.pkl'),
+    158: joblib.load('pickle_files/158.pkl'),
+    161: joblib.load('pickle_files/161.pkl'),
+    162: joblib.load('pickle_files/162.pkl'),
+    163: joblib.load('pickle_files/163.pkl'),
+    164: joblib.load('pickle_files/164.pkl'),
+    170: joblib.load('pickle_files/170.pkl'),
+    230: joblib.load('pickle_files/230.pkl'),
+    234: joblib.load('pickle_files/234.pkl'),
+    236: joblib.load('pickle_files/236.pkl'),
+    246: joblib.load('pickle_files/246.pkl'),
+    249: joblib.load('pickle_files/249.pkl'),
+    261: joblib.load('pickle_files/261.pkl')
+}
